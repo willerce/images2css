bb41f06fc95c17a54b78867df822ada2b589a2b7
willerce <willerce@gmail.com>
2015-02-02 16:49:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# images2css
+
+generate css from the existing sprite images
+
+## 使用
+
+```
+var kalok = images2css({
+    folderPath: ""
+    imageRelativePath: "../",
+    backgrounSize: true
+});
+
+fs.writeFile(path.join(process.cwd(), "kalok.css"), kalok);
+
+```
+
+参数说明：
+
+**folderPath**
+
+String 类型，需要遍历的图片目录，只遍历一层目录，不会遍历深层子目录
+默认值： process.cwd()
+
+**imageRelativePath**
+
+String 类型，background-image 的 url 相对路径
+默认值： "./"
+
+
+**backgroundSize**
+
+Boolean 类型，CSS 中是否需要带上 background-size 属性
+默认值： false
+
+## 联系我
+
+willerce(willerce@gmail.com)
---
+++ index.js
@@ -0,0 +1,72 @@
+var fs = require('fs');
+var path = require('path');
+var sizeOf = require('image-size');
+var css = require("node-css");
+
+function images2css(options) {
+
+    options = options || {};
+
+    options = {
+        folderPath: options.folderPath || process.cwd(),
+        imageRelativePath: options.imageRelativePath || "./",
+        backgrounSize: options.backgrounSize || false
+    };
+
+    //遍历目录下的 jpg, png
+    var files = fs.readdirSync(options.folderPath);
+
+    var cssContent = "";
+
+    files.forEach(function (file) {
+
+        if (file !== '.git' && file !== ".svn" && file !== ".idea") {
+            //判断是不是目录
+            var subFolderPath = path.join(options.folderPath, file);
+            var stat = fs.lstatSync(subFolderPath);
+            if (stat.isDirectory()) {
+
+                //在此目录遍历
+                var subfolders = fs.readdirSync(subFolderPath);
+
+                subfolders.forEach(function (image) {
+
+                    var imagePath = path.join(subFolderPath, image);
+                    var imageStat = fs.lstatSync(imagePath);
+
+                    if (imageStat.isFile() && ".png.jpg".indexOf(path.extname(imagePath)) >= 0) {
+
+                        //中~
+
+                        //计算高宽
+                        var dimensions = sizeOf(imagePath);
+                        var result = "";
+
+                        if (options.backgrounSize) {
+                            result = css("." + file + "__" + path.basename(imagePath, '.' + dimensions.type), {
+                                width: dimensions.width + "px",
+                                height: dimensions.height + "px",
+                                'background-image': 'url(' + options.imageRelativePath + file + '/' + image + ')',
+                                '-webkit-background-size': dimensions.width + "px" + " " + dimensions.height + "px",
+                                'background-size': dimensions.width + "px" + " " + dimensions.height + "px"
+                            });
+                        } else {
+                            result = css("." + file + "__" + path.basename(imagePath, '.' + dimensions.type), {
+                                width: dimensions.width + "px",
+                                height: dimensions.height + "px",
+                                'background-image': 'url(' + options.imageRelativePath + file + '/' + image + ')'
+                            });
+                        }
+
+                        cssContent = cssContent + result;
+
+                    }
+                });
+            }
+        }
+    });
+
+    return cssContent;
+}
+
+module.exports = images2css;
